a11b5271c0cfa0f7da1484719f9ff57a4310c439
Oliver Marriott <hello@omarriott.com>
2021-03-08 22:34:51 +1100

    Add initial variants

---
+++ index.js
@@ -0,0 +1,27 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(function({e, addVariant}) {
+  events_classes = [
+    ["phx-click", "phx-click-loading"],
+    ["phx-change", "phx-change-loading"],
+    ["phx-submit", "phx-submit-loading"],
+    ["phx-focus", "phx-focus-loading"],
+    ["phx-blur", "phx-blur-loading"],
+    ["phx-keydown", "phx-keydown-loading"],
+    ["phx-keyup", "phx-keyup-loading"],
+    ["phx-connected", "phx-connected"],
+    ["phx-disconnected", "phx-disconnected"],
+    ["phx-error", "phx-error"],
+  ]
+
+  for (let [event, cls] of events_classes) {
+    addVariant(event, ({ modifySelectors, separator }) => {
+      modifySelectors(({ className }) => {
+        return [
+          `.${cls} .${e(`phx-click${separator}${className}`)}`,
+          `.${cls}.${e(`phx-click${separator}${className}`)}`
+        ]
+      })
+    })
+  }
+})
